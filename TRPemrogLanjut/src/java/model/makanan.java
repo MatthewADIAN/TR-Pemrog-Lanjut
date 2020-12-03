f25c0c42338b1f30ce399d3b0b3a32d92211e344
MatthewADIAN <672018055@student.uksw.edu>
2020-12-03 15:00:01 +0700

    model makanan

---
+++ TRPemrogLanjut/src/java/model/makanan.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author USER
+ */
+public class makanan {
+    private int id;
+    private String nama;
+    private int stok;
+    private String supplier;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+         
+}
