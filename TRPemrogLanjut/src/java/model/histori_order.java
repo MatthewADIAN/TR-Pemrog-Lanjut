fd90527821f0e691a4be92eb5be2b024de5af3be
yogdrax <68434751+yogdrax@users.noreply.github.com>
2020-12-03 15:10:14 +0700

    model histori makanan

---
+++ TRPemrogLanjut/src/java/model/histori_order.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author YOGA
+ */
+public class histori_order {
+    private int id;
+    private String vendor;
+    private String nama_makanan;
+    private int jumlah;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getNama_makanan() {
+        return nama_makanan;
+    }
+
+    public void setNama_makanan(String nama_makanan) {
+        this.nama_makanan = nama_makanan;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+    
+    
+}
